cd32fe4a6135ac7d5d01e89bd020eb5c8e3fcdda
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-05-14 20:05:39 +0100

    chore: add custom configs

--- config/custom.js
+++ config/custom.js
@@ -17,4 +17,8 @@ module.exports.custom = {
   // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
   // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
   // …
+  emailVerificationCodeTTL: 2 * 60 * 60 * 1000, // 2 hours
+  passwordResetCodeTTL: 24 * 60 * 60 * 1000, // 24 hours
+  rememberMeCookieMaxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+  baseUrl: 'http://localhost:1337',
 }
