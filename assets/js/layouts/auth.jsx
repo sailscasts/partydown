5ec26653a1852419630a51e57f4dc3245eda1355
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-05-15 07:01:51 +0100

    fix: update margin-top

--- assets/js/layouts/auth.jsx
+++ assets/js/layouts/auth.jsx
@@ -3,7 +3,10 @@ export default function AuthLayout({ children, title }) {
   return (
     <>
       <Head title={`${title} · Partydown`} />
-      <main className="mx-auto w-3/12 mt-28">{children}</main>
+      <main className="mx-auto mt-28 w-3/12">
+        <h1 className="mb-3 text-center font-serif text-4xl">Partydown</h1>
+        {children}
+      </main>
     </>
   )
 }
