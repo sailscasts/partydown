41619dd0f9f6a631740bb50cff60b56128ffcfc7
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-05-15 08:35:57 +0100

    fix: update from address and name

--- config/mail.js
+++ config/mail.js
@@ -42,7 +42,7 @@ module.exports.mail = {
    *
    */
   from: {
-    address: 'hello@hagfish.io',
-    name: 'Kelvin from Hagfish',
+    address: 'hello@sailsconf.com',
+    name: 'Kelvin from Partydown',
   },
 }
